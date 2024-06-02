e1cc4be8a81f16a385761a14d46edd37a3dd987a
Paweł Cichowski <cichowski.pawel1@gmail.com>
2024-06-02 17:03:37 +0200

    resnet.cpp: add second half of the architecture
    Signed-off-by: Paweł Cichowski <cichowski.pawel1@gmail.com>

--- resnet.cpp
+++ resnet.cpp
@@ -20,23 +20,20 @@ const int BATCH_SIZE = 16;
  * tensor size, true by default
  */
 void resblock(
-    engine::kind engineKind,
-    memory::dims convSourceDims,
-    memory::dims convDestDims,
-    int convFilters,
-    bool downSample = true
+    engine::kind engineKind
 ) {
+    // TODO WIP, these should be function params
+    int convFilters = 64;
+    memory::dims convSourceDims = {BATCH_SIZE, 3, 224, 224};
+    memory::dims convDestDims {BATCH_SIZE, convFilters, 224, 224};
+    bool downSample = false;
+
     using tag = memory::format_tag;
     using dt = memory::data_type;
 
     auto eng = engine(engineKind, 0);
 
     stream s(eng);
-
-    // TODO vvvv test purposes only
-    convSourceDims = {BATCH_SIZE, 3, 224, 224};
-    convDestDims = {BATCH_SIZE, convFilters, 112, 112};
-    // TODO ^^^^ 
 
     // network primitives and their execute args
     std::vector<primitive> netForward, netBackward;
@@ -57,6 +54,9 @@ void resblock(
     // -----------------------------
     //     1st convolution layer
     // -----------------------------
+
+    // TODO add skip connection
+    // TODO check if downsampling works
 
     // 3x3 convolution weights and biases initialized to sin(i) (nonzero)
     memory::dims conv1WeightsDims = {
@@ -131,9 +131,9 @@ void resblock(
     // -------------------------------
     memory::dims batchNorm1Dims = {BATCH_SIZE, convFilters, convDestDims.at(2), convDestDims.at(3)};
 
-    const float epsilon = 0.00001;
+    const float epsilon1 = 0.00001;
     auto batchNorm1PrimitiveDesc = batch_normalization_forward::primitive_desc(
-        eng, prop_kind::forward, conv1PrimitiveDesc.dst_desc(), conv1PrimitiveDesc.dst_desc(), epsilon, normalization_flags::none
+        eng, prop_kind::forward, conv1PrimitiveDesc.dst_desc(), conv1PrimitiveDesc.dst_desc(), epsilon1, normalization_flags::none
     );
 
     auto batchNorm1DestMemory = memory(batchNorm1PrimitiveDesc.dst_desc(), eng);
@@ -148,11 +148,11 @@ void resblock(
     //     1st RELU
     // -------------------------------
     memory::dims relu1Dims = {BATCH_SIZE, convFilters, convDestDims.at(2), convDestDims.at(3)};
-    const float negativeSlope = 0.0f;
+    const float negativeSlope1 = 0.0f;
 
     auto relu1PrimitiveDesc = eltwise_forward::primitive_desc(
         eng, prop_kind::forward, algorithm::eltwise_relu,
-        conv1PrimitiveDesc.dst_desc(), conv1PrimitiveDesc.dst_desc(), negativeSlope);
+        conv1PrimitiveDesc.dst_desc(), conv1PrimitiveDesc.dst_desc(), negativeSlope1);
 
     auto relu1DestMemory = memory(relu1PrimitiveDesc.dst_desc(), eng);
 
@@ -162,8 +162,142 @@ void resblock(
         {DNNL_ARG_DST, relu1DestMemory}
     });
 
+    // -------------------------------
+    //     2nd conv layer
+    // -------------------------------
+    // TODO copy memory from 1st layer dest
+
+    // 1st conv produces tensors of dims (if not downsampling)
+    // [batch, 3, 224] x [64, 3, 3, 3] -> [batch, 64, 224, 224]
+    // 2nd conv produces
+    // [batch, 64, 224, 224] x [64, 64, 3, 3] -> [batch, 64, 224, 224]
+
+    memory::dims conv2SourceDims = {convSourceDims.at(0), convFilters, 
+        downSample ? convSourceDims.at(2) / 2 : convSourceDims.at(2),
+        downSample ? convSourceDims.at(3) / 2 : convSourceDims.at(3)};
+    memory::dims conv2WeightsDims = {convFilters, convFilters, 3, 3};
+    memory::dims conv2BiasDims = {convFilters};
+    memory::dims conv2Strides = {1, 1};
+    memory::dims conv2Padding = {1, 1};
+    
+    std::vector<float> conv2InitSourceMemory(product(conv2SourceDims));
+    for (size_t i = 0; i < conv2InitSourceMemory.size(); ++i) {
+        conv2InitSourceMemory[i] = sinf(float(i));
+    }
+    std::vector<float> conv2Weights(product(conv2WeightsDims));
+    for (size_t i = 0; i < conv2Weights.size(); ++i) {
+        conv2Weights[i] = sinf(float(i));
+    }
+    std::vector<float> conv2Bias(product(conv2BiasDims));
+    for (size_t i = 0; i < conv2Bias.size(); ++i) {
+        conv2Bias[i] = sinf(float(i));
+    }
+
+    // memory for tensor data
+    auto conv2UserSourceMemory = 
+        memory({{conv2SourceDims}, dt::f32, tag::nchw}, eng);
+    auto conv2UserWeightsMemory =
+        memory({{conv2WeightsDims}, dt::f32, tag::oihw}, eng);
+    auto conv2BiasMemory =
+        memory({{conv2BiasDims}, dt::f32, tag::x}, eng);
+    
+    write_to_dnnl_memory((void *) conv2InitSourceMemory.data(), conv2UserSourceMemory);
+    write_to_dnnl_memory((void *) conv2Weights.data(), conv2UserWeightsMemory);
+    write_to_dnnl_memory(conv2Bias.data(), conv2BiasMemory);
+
+    // memory descriptors for convolution data
+    auto conv2SourceMD = memory::desc({conv2SourceDims}, dt::f32, tag::any);
+    auto conv2DestMD = memory::desc({convDestDims}, dt::f32, tag::any);
+    auto conv2WeightsMD = memory::desc({conv2WeightsDims}, dt::f32, tag::any);
+    auto conv2BiasMD = memory::desc({conv2BiasDims}, dt::f32, tag::any);
+
+    // convolution primitive descriptor
+    auto conv2PrimitiveDesc = convolution_forward::primitive_desc(
+        eng, prop_kind::forward, algorithm::convolution_direct,
+        conv2SourceMD, conv2WeightsMD, conv2BiasMD,
+        conv2DestMD, conv2Strides, conv2Padding, conv2Padding
+    );
+
+    // reorder primitives between input and conv src if needed (??)
+    auto conv2SourceMemory = conv2UserSourceMemory;
+    if (conv2PrimitiveDesc.src_desc() != conv2UserSourceMemory.get_desc()) {
+        conv2UserSourceMemory = memory(conv2PrimitiveDesc.src_desc(), eng);
+        netForward.push_back(reorder(conv2UserSourceMemory, conv2SourceMemory));
+        netForwardArgs.push_back({{DNNL_ARG_FROM, conv2UserSourceMemory}, {DNNL_ARG_TO, conv2SourceMemory}});
+    }
+
+    auto conv2WeightsMemory = conv2UserWeightsMemory;
+    if (conv2PrimitiveDesc.weights_desc() != conv2UserWeightsMemory.get_desc()) {
+        conv2WeightsMemory = memory(conv2PrimitiveDesc.weights_desc(), eng);
+        netForward.push_back(reorder(conv2UserWeightsMemory, conv2WeightsMemory));
+        netForwardArgs.push_back({{DNNL_ARG_FROM, conv2UserWeightsMemory}, {DNNL_ARG_TO, conv2WeightsMemory}});
+    }
+
+    // memory for conv destination
+    auto conv2DestMemory = memory(conv2PrimitiveDesc.dst_desc(), eng);
+
+    // add convolution primitive to network forward
+    netForward.push_back(convolution_forward(conv2PrimitiveDesc));
+    netForwardArgs.push_back({
+        {DNNL_ARG_WEIGHTS, conv2WeightsMemory},
+        {DNNL_ARG_BIAS, conv2BiasMemory},
+        {DNNL_ARG_DST, conv2DestMemory}
+    });
+
+    // -------------------------------
+    //     2nd batch normalization
+    // -------------------------------
+    memory::dims batchNorm2Dims = {BATCH_SIZE, convFilters, convDestDims.at(2), convDestDims.at(3)};
+
+    const float epsilon2 = 0.00001;
+    auto batchNorm2PrimitiveDesc = batch_normalization_forward::primitive_desc(
+        eng, prop_kind::forward, conv2PrimitiveDesc.dst_desc(), conv2PrimitiveDesc.dst_desc(), epsilon2, normalization_flags::none
+    );
+
+    auto batchNorm2DestMemory = memory(batchNorm2PrimitiveDesc.dst_desc(), eng);
+
+    netForward.push_back(batch_normalization_forward(batchNorm2PrimitiveDesc));
+    netForwardArgs.push_back({
+        {DNNL_ARG_SRC, conv2DestMemory},
+        {DNNL_ARG_DST, batchNorm2DestMemory}
+    });
+
+    // -------------------------------
+    //     2nd RELU
+    // -------------------------------
+    memory::dims relu2Dims = {BATCH_SIZE, convFilters, convDestDims.at(2), convDestDims.at(3)};
+    const float negativeSlope2 = 0.0f;
+
+    auto relu2PrimitiveDesc = eltwise_forward::primitive_desc(
+        eng, prop_kind::forward, algorithm::eltwise_relu,
+        conv2PrimitiveDesc.dst_desc(), conv2PrimitiveDesc.dst_desc(), negativeSlope2);
+
+    auto relu2DestMemory = memory(relu2PrimitiveDesc.dst_desc(), eng);
+
+    netForward.push_back(eltwise_forward(relu2PrimitiveDesc));
+    netForwardArgs.push_back({
+        {DNNL_ARG_SRC, batchNorm2DestMemory},
+        {DNNL_ARG_DST, relu2DestMemory}
+    });
 
 
+    // TODO add backward stream
+
+    // check if we forgot something
+    assert(netForward.size() == netForwardArgs.size() && "Something is missing in forward network stream");
+
+    int trainingIterations = 1;
+    while (trainingIterations)  {
+        // forward
+        for (size_t i = 0; i < netForward.size(); ++i) {
+            netForward.at(i).execute(s, netForwardArgs.at(i));
+        }
+    }
+
+    s.wait();
+}
 
 
+int main(int argc, char **argv) {
+    return handle_example_errors(resblock, parse_engine_kind(argc, argv));
 }
